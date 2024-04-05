cd280a6b1371585206c6cbb29341cb1db38ccbdc
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-04-05 13:01:52 +0200

    Client class refactor and move exceptions to controller

--- src/main/java/com/example/pslab1fxclient/Client.java
+++ src/main/java/com/example/pslab1fxclient/Client.java
@@ -14,37 +14,20 @@ public class Client {
 
     public Client(ClientController c) { this.c = c; }
 
-    public void connect(String ip, Integer port) {
-        try {
-            socket = new Socket(ip, port);
-            c.sendAlert("Connected");
-            output = new DataOutputStream(socket.getOutputStream());
-            input = new DataInputStream(socket.getInputStream());
-        } catch (UnknownHostException e) {
-            c.sendAlert("Unknown Host");
-        } catch (IOException e) {
-            c.sendAlert("IOException [connect]");
-        }
+    public void connect(String ip, Integer port) throws IOException {
+        socket = new Socket(ip, port);
+        output = new DataOutputStream(socket.getOutputStream());
+        input = new DataInputStream(socket.getInputStream());
     }
 
-    public void echo(String message) {
-        try {
-            output.writeUTF(message);
-            c.sendAlert("Message: " + message + " sent");
-
-            String echo = input.readUTF();
-            c.sendAlert("Server echo: " + echo);
-        } catch (IOException e) {
-            c.sendAlert("IOException [echo]");
-        }
+    public void echo(String message) throws IOException {
+        output.writeUTF(message);
+        c.sendAlert("Message: " + message + " sent");
+        String echo = input.readUTF();
+        c.sendAlert("Server echo: " + echo);
     }
 
-    public void disconnect() {
-        try {
-            socket.close();
-            c.sendAlert("Disconnected");
-        } catch (IOException e) {
-            c.sendAlert("IOException [disconnect]");
-        }
+    public void disconnect() throws IOException {
+        if (socket != null) socket.close();
     }
 }
