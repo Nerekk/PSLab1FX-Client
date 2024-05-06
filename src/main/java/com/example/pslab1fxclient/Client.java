bbf375b1546530720dc53dceb44a41295c343973
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-06 21:58:51 +0200

    Reading first validating message from server

--- src/main/java/com/example/pslab1fxclient/Client.java
+++ src/main/java/com/example/pslab1fxclient/Client.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import static com.example.pslab1fxclient.ClientController.ECHO;
+import static com.example.pslab1fxclient.ClientController.INFO;
 
 public class Client {
     private Socket socket;
@@ -14,12 +15,30 @@ public class Client {
     private DataInputStream input;
     private final ClientController c;
 
+    private static final int BUSY = 2;
+    private static final int ALLOWED = 1;
+    private static final int DENIED = 0;
+
     public Client(ClientController c) { this.c = c; }
 
     public void connect(String ip, Integer port) throws IOException {
         socket = new Socket(ip, port);
         output = new DataOutputStream(socket.getOutputStream());
         input = new DataInputStream(socket.getInputStream());
+
+        int validator = input.readInt();
+        if (validator == BUSY) {
+            c.sendAlert(INFO, "Server is busy. Try again later.");
+            socket.close();
+            output.close();
+            input.close();
+            throw new IOException();
+        } else if (validator == DENIED) {
+            socket.close();
+            output.close();
+            input.close();
+            throw new IOException();
+        }
     }
 
     public void echo(String message) throws IOException {
